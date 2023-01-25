529821afb0b844b065ec53269a2f2b56f0de6f8d
Den <workplacel@mail.ru>
2023-01-25 23:52:21 +0500

    1 Lesson

---
+++ program.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+/*Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+ * n! (произведение чисел от 1 до n)
+ */
+
+// public class program{
+//     public static void main(String[] args) {
+//         System.out.println("Введите число: ");
+//         Scanner iScanner = new Scanner(System.in);
+//         String N = iScanner.nextLine();
+//         int n = Integer.parseInt(N);
+//         iScanner.close();
+//         int resultPlus = n * (n + 1) / 2;
+        
+//         int resultMulty = 1;
+//         for (int i = 1; i <= n; i ++){
+//             resultMulty *= i;
+//         }
+//         System.out.println(resultPlus);
+//         System.out.println(resultMulty);
+
+//     }
+// }
+    public class program {
+
+      public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+          if (i % 1 == 0 || i % i == 0){
+            System.out.println(i);
+          }
+        }
+      }
+    }
